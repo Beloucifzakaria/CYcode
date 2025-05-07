477941bf09ccf089f3798b7710d74c87204b22ba
Yanis1505 <yanines1505@gmail.com>
2025-05-07 12:30:50 +0200

    recherche.h

---
+++ recherche.h
@@ -0,0 +1,9 @@
+#ifndef RECHERCHE_H
+#define RECHERCHE_H
+
+#include "structures.h" // Pour le type Animal
+
+// Fonction pour rechercher des animaux selon nom, espèce et âge
+void rechercher_animaux(const char *fichier);
+
+#endif
