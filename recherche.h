4539c38bb3a0847c328f742956708a97aa7049b3
anthonyancelin <antho.ancelin@gmail.com>
2025-05-11 23:05:37 +0200

    Delete recherche.h

--- recherche.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECHERCHE_H
-#define RECHERCHE_H
-
-#include "structures.h" // Pour le type Animal
-
-// Fonction pour rechercher des animaux selon nom, espèce et âge
-void rechercher_animaux(const char *fichier);
-
-#endif
